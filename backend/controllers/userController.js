82d67c4fcc6124a05c32cfae8f30a7c711676b18
Htut Aung Wai <defo19987@gmail.com>
2023-09-15 21:20:55 +0700

    added custom error middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,10 @@
+import expressAsyncHandler from "express-async-handler";
+
 // @desc Auth user/set token
 // router POST /api/users/auth
 // @access Public
-
-async function authUser(req, res) {
+const authUser = expressAsyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth user" });
-}
+});
 
 export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 const app = express();
-app.get("/", (req, res) => res.send("Server is Ready"));
+
 app.use("/api/users", userRoutes);
+app.get("/", (req, res) => res.send("Server is Ready"));
+
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(port, () => {
   console.log(`SERVER STARTED ON ${port}`);
 });
